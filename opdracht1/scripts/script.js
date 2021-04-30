f7365738e764b434f0cf52c7e42db6263f807b3d
milasteneker <70643876+milasteneker@users.noreply.github.com>
2021-04-30 17:24:20 +0200

    Add files via upload

--- opdracht1/scripts/script.js
+++ opdracht1/scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("howdy");
+
+var hapje = document.querySelector('#haribo');
+var hapjeSound = document.querySelector('#hapje');
+
+
+function hapjeSpeelt(event){
+	hapjeSound.play();
+}
+
+hapje.addEventListener('click', hapjeSpeelt );
