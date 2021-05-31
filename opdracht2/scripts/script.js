144c6c5112d2ca67a81dc0e2fa32b0a27981fa80
milasteneker <70643876+milasteneker@users.noreply.github.com>
2021-05-31 21:43:03 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,23 @@
 // JavaScript Document
 console.log("howdy");
+
+
+var deLijst = document.getElementById('list');
+var sortable = Sortable.create(deLijst);
+
+var optionAll = document.querySelector("#filter-all");
+var optionVormgeving = document.querySelector("#filter-vormgeving");
+var optionCode = document.querySelector("#filter-code");
+var optionOverig = document.querySelector("#filter-overig");
+
+function filterList(event){
+  let deLijst = document.querySelector("main");
+  let nieuweFilter = event.target.value;
+  deLijst.className = "";
+  deLijst.classList.add(nieuweFilter);
+}
+
+optionAll.addEventListener("change", filterList);
+optionVormgeving.addEventListener("change", filterList);
+optionCode.addEventListener("change", filterList);
+optionOverig.addEventListener("change", filterList);
